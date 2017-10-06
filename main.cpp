5d5e607522aa8ee007858abea620395dfe5d0dfd
KadrusBAG <32191410+KadrusBAG@users.noreply.github.com>
2017-10-06 11:42:39 +0300

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,82 @@
+#include <iostream>
+#include <sstream>
+using namespace std;
+
+bool proverka_mat(int mat[3][3]){
+    bool result=true;
+    for(int i=0; i<3; ++i){
+        string stroka;
+        getline(cin, stroka);
+        istringstream stream(stroka);
+        for(int j=0; j<3; ++j){
+            if(!(stream>>mat[i][j])){
+                result=false;
+                break;
+            }
+        }
+    }
+    return result;
+}
+
+int main()
+{
+    int mat1[3][3], mat2[3][3];
+    string op;
+    if(proverka_mat(mat1)){
+        getline(cin, op);
+        if(op=="+"){
+            if(proverka_mat(mat2)){
+                cout<<endl;
+                for(int i=0; i<3; ++i){
+                    for(int j=0; j<3; ++j){
+                        cout<<(mat1[i][j]+mat2[i][j])<<" ";
+                    }
+                    cout<<endl;
+                }
+            }
+            else{
+                cout<<"An error has occured while reading input data"<<endl;
+                return -1;
+            }
+        }
+        if(op=="-"){
+            if(proverka_mat(mat2)){
+                cout<<endl;
+                for(int i=0; i<3; ++i){
+                    for(int j=0; j<3; ++j){
+                        cout<<(mat1[i][j]-mat2[i][j])<<" ";
+                    }
+                    cout<<endl;
+                }
+            }
+            else{
+                cout<<"An error has occured while reading input data"<<endl;
+                return -1;
+            }
+        }
+        if(op=="*"){
+            if(proverka_mat(mat2)){
+                cout<<endl;
+                for(int i=0; i<3; ++i){
+                    for(int j=0; j<3; ++j){
+                        int result_op=0;
+                        for(int k=0; k<3; ++k){
+                            result_op+=mat1[i][k]*mat2[k][j];
+                        }
+                        cout<<result_op<<" ";
+                    }
+                }
+            }
+            else{
+                cout<<"An error has occured while reading input data"<<endl;
+                return -1;
+            }
+        }
+        
+    }
+    else{
+        cout<<"An error has occured while reading input data"<<endl;
+        return -1;
+    }
+return 0;
+}
